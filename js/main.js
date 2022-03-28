e371a2fdd04f0d260e2953001bcc7a9f736bdd23
danyip <hello@danyip.com>
2022-03-29 00:00:03 +1100

    contact dropdown

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('hello');
+
+const dropdownButton = document.querySelector('.dropdown-button')
+
+dropdownButton.addEventListener('click', ()=>{
+  console.log('clicked');
+  document.querySelector('.dropdown-content').classList.toggle('show')
+} )
+
+window.onclick = (e)=>{
+  if(!e.target.matches('.dropdown-button')){
+    const dropdowns = document.querySelectorAll('.dropdown-content');
+    dropdowns.forEach(dropdown=>{
+      if (dropdown.classList.contains('show')) {
+        dropdown.classList.remove('show')
+        }
+    })
+  }
+}
